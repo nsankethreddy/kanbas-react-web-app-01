b451095ef1891248e5489c56bd4f031de715b17f
nsankethreddy <sankethn1947@gmail.com>
2024-09-19 18:17:39 -0400

    add github

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -4,6 +4,7 @@ export default function TOC() {
   return (
     <ul>
       <li><Link to="/Labs">Labs</Link></li>
+      <li><Link id="wd-github" to="https://github.com/nsankethreddy/kanbas-react-web-app-01">GitHub</Link></li>
       <li><Link to="/Labs/Lab1">Lab 1</Link></li>
       <li><Link to="/Labs/Lab2">Lab 2</Link></li>
       <li><Link to="/Labs/Lab3">Lab 3</Link></li>
