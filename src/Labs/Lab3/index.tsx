4372000b55060b41377121e6350a4e4e5f1c19ca
nsankethreddy <sankethn1947@gmail.com>
2024-10-15 17:19:17 -0400

    labs

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -30,9 +30,11 @@ import Styles from "./Styles";
 import Add from "./Add";
 import Square from "./Square";
 import Highlight from "./Highlight";
-
+import AddPathParameters from "./AddPathParameters";
+import PathParameters from "./PathParameters";
 
 export default function Lab3() {
+  console.log('Hello World!');
   return (
     <div>
       <h2>Lab 3</h2>
@@ -72,6 +74,7 @@ export default function Lab3() {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
         vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
       </Highlight>
-    </div>
+      <PathParameters />
+    </div >
   );
 }
