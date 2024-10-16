85d7ec61cdceaa4904fd0e923fb61854822fae22
nsankethreddy <sankethn1947@gmail.com>
2024-10-15 22:05:20 -0400

    Assignments kinda

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -6,10 +6,15 @@ import { FaSortDown } from "react-icons/fa";
 import AssignmentControlButtons from "./AssignmentControlButtons";
 import A1ControlButtons from "./A1ControlButtons";
 import { GiNotebook } from "react-icons/gi";
+import { useParams } from "react-router";
+import * as db from "../../Database";
 
 
 
 export default function Assignments() {
+  const { cid } = useParams();
+  const assignments = db.assignments.filter(assignment => assignment.course === cid);
+
   return (
     <div id="wd-assignments" className="wd-assignment">
       <div id="wd-assignments-header" className="assignment-header">
@@ -30,9 +35,6 @@ export default function Assignments() {
             Assignment</button>
         </div>
       </div>
-
-
-
       <ul id="wd-assignments-list" className="list-group rounded-0">
         <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
           <div className="wd-title p-3 ps-2 bg-light">
@@ -42,45 +44,21 @@ export default function Assignments() {
             <AssignmentControlButtons />
           </div>
           <ul className="wd-lessons list-group rounded-0">
-            <li className="wd-lesson list-group-item p-3 ps-1 d-flex align-items-center" style={{ borderLeft: '5px solid green' }}>
-              <BsGripVertical className="me-3 fs-3" />
-              <GiNotebook className="me-3 fs-3" style={{ color: 'green' }} />
-              <div className="flex-grow-1 ms-3">
-                <h4 className="mb-0">
-                  <a href="#/Kanbas/Courses/1234/Assignments/123" className="text-decoration-none text-dark">A1</a>
-                </h4>
-                <p style={{ fontSize: '0.8em' }} className="mb-0">
-                  <span style={{ color: 'red' }}>Multiple Modules</span> | <span style={{ fontWeight: 'bold' }}>Not available until</span>  May 6 at 12:00am | <span style={{ fontWeight: 'bold' }}>Due</span>  May 13 at 11:59pm | 100 pts
-                </p>
-              </div>
-              <A1ControlButtons />
-            </li>
-            <li className="wd-lesson list-group-item p-3 ps-1 d-flex align-items-center" style={{ borderLeft: '5px solid green' }}>
-              <BsGripVertical className="me-3 fs-3" />
-              <GiNotebook className="me-3 fs-3" style={{ color: 'green' }} />
-              <div className="flex-grow-1 ms-3">
-                <h4 className="mb-0">
-                  <a href="#/Kanbas/Courses/1234/Assignments/123" className="text-decoration-none text-dark">A2</a>
-                </h4>
-                <p style={{ fontSize: '0.8em' }} className="mb-0">
-                  <span style={{ color: 'red' }}>Multiple Modules</span> | <span style={{ fontWeight: 'bold' }}>Not available until</span> May 16 at 12:00am | <span style={{ fontWeight: 'bold' }}>Due</span> May 23 at 11:59pm | 100 pts
-                </p>
-              </div>
-              <A1ControlButtons />
-            </li>
-            <li className="wd-lesson list-group-item p-3 ps-1 d-flex align-items-center" style={{ borderLeft: '5px solid green' }}>
-              <BsGripVertical className="me-3 fs-3" />
-              <GiNotebook className="me-3 fs-3" style={{ color: 'green' }} />
-              <div className="flex-grow-1 ms-3">
-                <h4 className="mb-0">
-                  <a href="#/Kanbas/Courses/1234/Assignments/123" className="text-decoration-none text-dark">A3</a>
-                </h4>
-                <p style={{ fontSize: '0.8em' }} className="mb-0">
-                  <span style={{ color: 'red' }}>Multiple Modules</span> | <span style={{ fontWeight: 'bold' }}>Not available until</span>  May 26 at 12:00am | <span style={{ fontWeight: 'bold' }}>Due</span> May 31 at 11:59pm | 100 pts
-                </p>
-              </div>
-              <A1ControlButtons />
-            </li>
+            {assignments.map(assignment => (
+              <li key={assignment._id} className="wd-lesson list-group-item p-3 ps-1 d-flex align-items-center" style={{ borderLeft: '5px solid green' }}>
+                <BsGripVertical className="me-3 fs-3" />
+                <GiNotebook className="me-3 fs-3" style={{ color: 'green' }} />
+                <div className="flex-grow-1 ms-3">
+                  <h4 className="mb-0">
+                    <a href={`#/Kanbas/Courses/${cid}/Assignments/${assignment._id}`} className="text-decoration-none text-dark">{assignment.title}</a>
+                  </h4>
+                  <p style={{ fontSize: '0.8em' }} className="mb-0">
+                    <span style={{ color: 'red' }}>{assignment.title}</span> | <span style={{ fontWeight: 'bold' }}>Not available until</span> {assignment.availableDate} | <span style={{ fontWeight: 'bold' }}>Due</span> {assignment.dueDate} | {assignment.points} pts
+                  </p>
+                </div>
+                <A1ControlButtons />
+              </li>
+            ))}
           </ul>
         </li>
       </ul >
