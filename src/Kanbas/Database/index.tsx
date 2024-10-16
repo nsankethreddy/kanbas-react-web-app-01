cbb83a10ea409862b7fc74d34c88bdd470c3aff3
nsankethreddy <sankethn1947@gmail.com>
2024-10-15 21:28:26 -0400

    Modules

--- src/Kanbas/Database/index.tsx
+++ src/Kanbas/Database/index.tsx
@@ -1,3 +1,6 @@
 import courses from "./courses.json";
 import modules from "./modules.json";
-export { courses, modules };
+import assignments from "./assignments.json";
+export {
+    courses, modules, assignments,
+};
