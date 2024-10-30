20e0569d5c15a23219549025362ddcadc6995b5e
nsankethreddy <sankethn1947@gmail.com>
2024-10-29 22:29:18 -0400

    3.3.2

--- src/Kanbas/Courses/Modules/ModulesControlButtons.tsx
+++ src/Kanbas/Courses/Modules/ModulesControlButtons.tsx
@@ -1,9 +1,14 @@
 import { IoEllipsisVertical } from "react-icons/io5";
 import GreenCheckmark from "./GreenCheckmark";
 import { BsPlus } from "react-icons/bs";
-export default function ModulesControlButtons() {
+import { FaTrash } from "react-icons/fa";
+
+export default function ModulesControlButtons(
+    { moduleId, deleteModule }: { moduleId: string; deleteModule: (moduleId: string) => void; }) {
+
     return (
         <div className="float-end">
+            <FaTrash className="text-danger me-2 mb-1" onClick={() => deleteModule(moduleId)} />
             <GreenCheckmark />
             <BsPlus className="fs-2 me-2" />
             <IoEllipsisVertical className="fs-4" />
--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -5,13 +5,27 @@ import LessonControlButtons from "./LessonControlButtons";
 import ModulesControlButtons from "./ModulesControlButtons";
 import { useParams } from "react-router";
 import * as db from "../../Database";
+import React, { useState } from "react";
 
 export default function Modules() {
   const { cid } = useParams();
-  const modules = db.modules;
+  const [modules, setModules] = useState<any[]>(db.modules);
+  const [moduleName, setModuleName] = useState("");
+  const addModule = () => {
+    setModules([...modules, {
+      _id: new Date().getTime().toString(),
+      name: moduleName, course: cid, lessons: []
+    }]);
+    setModuleName("");
+  };
+  const deleteModule = (moduleId: string) => {
+    setModules(modules.filter((m) => m._id !== moduleId));
+  };
+
+
   return (
     <div>
-      <ModulesControls /><br /><br /><br /><br />
+      <ModulesControls setModuleName={setModuleName} moduleName={moduleName} addModule={addModule} /><br /><br /><br /><br />
       <ul id="wd-modules" className="list-group rounded-0">
         {modules
           .filter((module: any) => module.course === cid)
@@ -20,7 +34,9 @@ export default function Modules() {
               <div className="wd-title p-3 ps-2 bg-secondary">
                 <BsGripVertical className="me-2 fs-3" />
                 {module.name}
-                <ModulesControlButtons />
+                <ModulesControlButtons
+                  moduleId={module._id}
+                  deleteModule={deleteModule} />
               </div>
               {module.lessons && (
                 <ul className="wd-lessons list-group rounded-0">
