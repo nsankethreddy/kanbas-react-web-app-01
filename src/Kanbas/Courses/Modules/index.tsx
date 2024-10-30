48686299c3901646516a63f048f6a8c4e80e7ef1
nsankethreddy <sankethn1947@gmail.com>
2024-10-29 22:45:34 -0400

    3.3.3

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -22,21 +22,36 @@ export default function Modules() {
     setModules(modules.filter((m) => m._id !== moduleId));
   };
 
+  const editModule = (moduleId: string) => {
+    setModules(modules.map((m) => (m._id === moduleId ? { ...m, editing: true } : m)));
+  };
+  const updateModule = (module: any) => {
+    setModules(modules.map((m) => (m._id === module._id ? module : m)));
+  };
 
   return (
     <div>
       <ModulesControls setModuleName={setModuleName} moduleName={moduleName} addModule={addModule} /><br /><br /><br /><br />
       <ul id="wd-modules" className="list-group rounded-0">
-        {modules
-          .filter((module: any) => module.course === cid)
-          .map((module: any) => (
+        {modules.filter((module: any) => module.course === cid).map((module: any) => (
             <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
               <div className="wd-title p-3 ps-2 bg-secondary">
                 <BsGripVertical className="me-2 fs-3" />
-                {module.name}
+                {!module.editing && module.name}
+                {module.editing && (
+                  <input className="form-control w-50 d-inline-block"
+                    onChange={(e) => updateModule({ ...module, name: e.target.value })}
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") {
+                        updateModule({ ...module, editing: false });
+                      }
+                    }}
+                    defaultValue={module.name} />
+                )}
                 <ModulesControlButtons
                   moduleId={module._id}
-                  deleteModule={deleteModule} />
+                  deleteModule={deleteModule}
+                  editModule={editModule} />
               </div>
               {module.lessons && (
                 <ul className="wd-lessons list-group rounded-0">
