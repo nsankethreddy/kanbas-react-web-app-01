12d2e43f20bfe5513e5199721dac3901ab895003
nsankethreddy <sankethn1947@gmail.com>
2024-11-05 12:22:45 -0500

    remove console logs

--- src/Kanbas/Dashboard/reducer.ts
+++ src/Kanbas/Dashboard/reducer.ts
@@ -20,23 +20,14 @@ const enrollmentSlice = createSlice({
       state,
       action: PayloadAction<{ user: string; course: string }>
     ) {
-      console.log("Before enrollCourse:",JSON.stringify(state.enrollments, null, 2));
-      console.log("New enrollment:", JSON.stringify(action.payload, null, 2));
-
       const newEnrollment: Enrollment = { _id: nanoid(), ...action.payload };
 
       state.enrollments.unshift(newEnrollment);
-
-      console.log("After enrollCourse:",JSON.stringify(state.enrollments, null, 2));
     },
     unenrollCourse(
       state,
       action: PayloadAction<{ user: string; course: string }>
     ) {
-      console.log("Before unenrollCourse:",JSON.stringify(state.enrollments, null, 2));
-      console.log("Unenrollment request:",JSON.stringify(action.payload, null, 2));
-
-      // Filter out the enrollment that matches the user and course from action payload
       state.enrollments = state.enrollments.filter(
         (enrollment: Enrollment) =>
           !(
@@ -44,8 +35,6 @@ const enrollmentSlice = createSlice({
             enrollment.course === action.payload.course
           )
       );
-
-      console.log("After unenrollCourse:",JSON.stringify(state.enrollments, null, 2));
     },
   },
 });
