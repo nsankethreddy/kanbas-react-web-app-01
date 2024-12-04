376d11d0bbadaceb052de98ffd2c7a206ce1f908
nsankethreddy <sankethn1947@gmail.com>
2024-12-04 18:37:54 -0500

    try

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -27,7 +27,7 @@ import TernaryOperator from "./TernaryOperator";
 import VariablesAndConstants from "./VariablesAndConstants";
 import VariableTypes from "./VariableTypes";
 import Add from "./Add";
-import Square from "./square";
+import Square from "./Square";
 import Highlight from "./Highlight";
 import { useSelector } from "react-redux";
 
