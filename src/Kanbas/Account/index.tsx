9ce4c3fd3fb549740876406b35337e6d83f71625
nsankethreddy <sankethn1947@gmail.com>
2024-12-04 01:03:48 -0500

    signin

--- src/Kanbas/Account/index.tsx
+++ src/Kanbas/Account/index.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Profile from "./Profile";
-import Signin from "./signin";
+import Signin from "./Signin";
 import Signup from "./Signup";
 import AccountNavigation from "./Navigation";
 import { useSelector } from "react-redux";
@@ -16,7 +16,7 @@ export default function Account() {
           <td valign="top">
             <Routes>
               <Route path="/"
-                     element={<Navigate to={ currentUser ? "/Kanbas/Account/Profile" : "/Kanbas/Account/Signin" } />} />
+                element={<Navigate to={currentUser ? "/Kanbas/Account/Profile" : "/Kanbas/Account/Signin"} />} />
               <Route path="/Signin" element={<Signin />} />
               <Route path="/Profile" element={<Profile />} />
               <Route path="/Signup" element={<Signup />} />
